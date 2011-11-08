49a603de47a016298e794781ca75bc1cd3e16cfe
Christian Walde <walde.christian@googlemail.com>
2011-11-08 13:20:28 +0100

    cut out more irrelevant code

--- Pod/PseudoPod/DOM/Role/LaTeX.pm
+++ Pod/PseudoPod/DOM/Role/LaTeX.pm
@@ -1,24 +1,17 @@
 package Pod::PseudoPod::DOM::Role::LaTeX;
 
 my %characters = (
-    acute   => sub { qq|\\'| . shift },
-    grave   => sub { qq|\\`| . shift },
-    uml     => sub { qq|\\"| . shift },
-    cedilla => sub { '\c' },              # ccedilla
-    opy     => sub { '\copyright' },      # copy
-    dash    => sub { '---' },             # mdash
-    lusmn   => sub { '\pm' },             # plusmn
-    mp      => sub { '\&' },              # amp
+    acute => sub {
+        qq|\\'| . shift
+    },
+    grave => sub {
+        qq|\\`| . shift;
+    },
 );
 
-sub emit_character
-{
-    my $content = "aacute";
-
-    if (my ($char, $class) = $content =~ /(\w)(\w+)/)
-    {
-        return $characters{$class}->($char) if exists $characters{$class};
-    }
+sub emit_character {
+    $characters{'acute'}->( 'a' );
+    $characters{'grave'}->( 'a' );
 }
 
 1;
