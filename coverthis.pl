188fa333669ab13303d10c25ad2ed19f9b815023
Christian Walde <walde.christian@googlemail.com>
2011-11-08 13:28:44 +0100

    cut down coverage script to essentials

--- coverthis.pl
+++ coverthis.pl
@@ -3,15 +3,6 @@
 use strict;
 use warnings;
 
-=pod
-    Plack/Session
-    Plack/Request.pm
-    Sub/Quote.pm
-=cut
-
-my @select = qw(
-);
-
 system( "cover -delete" );
-system( "perl -MDevel::Cover" . ( @select ? "=-select," . join( ",", @select ) : "" ) . " tags.t" );
+system( "perl -MDevel::Cover tags.t" );
 system( "cover" );
